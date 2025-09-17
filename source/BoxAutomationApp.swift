213754fd8e9496f39ba906b20117881b8e37c8cb
let-me-isekai-u <sllslls2002@gmail.com>
2025-09-18 00:31:41 +0700

    init iOS app + workflow

---
+++ source/BoxAutomationApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BoxAutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ source/ContentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("BoxAutomation").font(.title).bold()
+            Text("Hello iPhone jailbreak!").foregroundColor(.secondary)
+        }
+        .padding()
+    }
+}
